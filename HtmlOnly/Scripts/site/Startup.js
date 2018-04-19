7c061599c1828e487446f751e8fa35be869bfc7a
jason-haught <jason_haught@hotmail.com>
2018-04-18 23:19:14 -0400

    Facebook Support

--- HtmlOnly/Scripts/site/Startup.js
+++ HtmlOnly/Scripts/site/Startup.js
@@ -4,30 +4,57 @@
 
 // asyncrhonously fetch the html template partial from the file directory,
 // then set its contents to the html of the parent element
-function loadHTML(url, id) {
-    req = new XMLHttpRequest();
-    req.open('GET', url);
-    req.send();
-    req.onload = () => {
-        //$id(id).innerHTML = req.responseText;
-        $("#" + id).html(req.responseText); //allow scripts
-    };
+//function loadHTML(url, id) {
+//    req = new XMLHttpRequest();
+//    req.open('GET', url);
+//    req.send();
+//    req.onload = ()=> {
+//        //$id(id).innerHTML = req.responseText;
+//        $("#" + id).html(req.responseText); //allow scripts
+//    };
+//}
+
+function loadHtmlAsync(url, id, isCache)
+{
+    var isCacheCall = false;
+    if (isCache) { isCacheCall = isCache; }
+
+    $.ajax({
+        method: "GET",
+        cache: isCacheCall,
+        url: url,
+        success: function (result) {
+            $("#" + id).html(result);
+        }
+    });
 }
 
 // use #! to hash
 router = new Navigo(null, true, '#!');
+//router.on({
+//    // 'view' is the id of the div element inside which we render the HTML
+//    'home': () => { loadHtmlAsync('./templates/home.html', 'view'); },
+//    'about': () => { loadHtmlAsync('./templates/about.html', 'view'); },
+//    'facebook': () => { loadHtmlAsync('./templates/facebook.html', 'view'); },
+//    'blog': () => { loadHtmlAsync('./templates/blog.html', 'view'); }
+//});
+
 router.on({
     // 'view' is the id of the div element inside which we render the HTML
-    'home': () => { loadHTML('./templates/home.html', 'view'); },
-    'about': () => { loadHTML('./templates/about.html', 'view'); },
-    'blog': () => { loadHTML('./templates/blog.html', 'view'); }
+    'home': function() { loadHtmlAsync('./templates/home.html', 'view', false); },
+    'about': function () { loadHtmlAsync('./templates/about.html', 'view', true); },
+    'facebook': function () { loadHtmlAsync('./templates/facebook.html', 'view', false); },
+    'blog': function () { loadHtmlAsync('./templates/blog.html', 'view', false); }
 });
 
 // set the default route
 //router.on(() => { $id('view').innerHTML = '<h2>Home Page Default route</h2>'; });
-router.on(() => { loadHTML('./templates/home.html', 'view'); });
+//router.on(() => { loadHtmlAsync('./templates/home.html', 'view'); });
+
+router.on(function() { loadHtmlAsync('./templates/home.html', 'view'); });
 
 // set the 404 route
-router.notFound((query) => { $id('view').innerHTML = '<h3>Couldn\'t find the page you\'re looking for...</h3>'; });
+//router.notFound((query) => { $id('view').innerHTML = '<h3>Couldn\'t find the page you\'re looking for...</h3>'; });
+router.notFound(function (query){ $id('view').innerHTML = '<h3>Couldn\'t find the page you\'re looking for...</h3>'; });
 
 router.resolve();
